300adb1d080d7a207d44fd3d547cb925a2f64be4
Sage <142645060+SageCreates@users.noreply.github.com>
2024-05-09 13:24:43 -0400

    Add files via upload

---
+++ zellijinstall.sh
@@ -0,0 +1,4 @@
+wget https://github.com/zellij-org/zellij/releases/download/v0.31.3/zellij-x86_64-unknown-linux-musl.tar.gz
+tar -xvf zellij-x86_64-unknown-linux-musl.tar.gz
+chmod +x zellij
+mv zellij /bin

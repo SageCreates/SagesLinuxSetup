4811b0b6381a4bdf0cde9a633b58926736b9093e
Sage <142645060+SageCreates@users.noreply.github.com>
2024-07-06 20:37:49 -0600

    Update zellijinstall.sh

--- zellijinstall.sh
+++ zellijinstall.sh
@@ -1,4 +1,4 @@
 wget https://github.com/zellij-org/zellij/releases/download/v0.31.3/zellij-x86_64-unknown-linux-musl.tar.gz
 tar -xvf zellij-x86_64-unknown-linux-musl.tar.gz
-chmod +x zellij
-mv zellij /bin
+sudo chmod +x zellij
+sudo mv zellij /bin
